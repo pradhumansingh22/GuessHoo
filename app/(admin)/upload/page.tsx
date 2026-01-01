2e90df8b90f306c3311ca69baa393f3b8d0b5fa1
prahumansingh22 <pradhuman362@gmail.com>
2026-01-01 17:05:10 +0530

    image

--- app/(admin)/upload/page.tsx
+++ app/(admin)/upload/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 
 import { useState } from "react";
+import bcrypt from "bcrypt";
 
 export default function() {
     const [file, setFile] = useState<File | null>();
     const formData = new FormData();
+   const [password, setPassword] = useState("");
 
     const handleUpload = async () => {
+
         
-         formData.append("file", file!);
+        formData.append("file", file!);
+        formData.append("poolName", "test");
+        formData.append("imageName", "test-image")
+        formData.append("password", password);
         console.log(formData.get("file"));
         const res = await fetch("http://localhost:8080/admin/img/upload", {
           method: "POST",
@@ -25,5 +31,6 @@ export default function() {
     return <div>
         <input type="file" placeholder="Upload an Image" onInput={(e: any) => { setFile(e.target.files[0]) }} />
         <button className="bg-amber-400 text-amber-50 p-5" onClick={handleUpload}> Upload </button>
+        <input type="password" placeholder="enter password" onChange={(e)=>{setPassword(e.target.value)}} />
     </div>
 }
